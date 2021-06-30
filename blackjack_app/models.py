70ceaca2e6d6383c408e89990263d2eae0d12fa0
ricardogc92 <ricardogc92@hotmail.com>
2021-06-29 23:39:03 -0700

    right before migrations testing new python logic

--- blackjack_app/models.py
+++ blackjack_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+import random
 # Create your models here.
+class Game(models.Model):
+    num_decks = models.IntegerField()
+    deck = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+    user_cards=[]
+    comp_cards=[]
+
+    def shuffle(self,deck):
+        self.deck = random.shuffle(deck)
+        return
+    def hit(self):
+        card=self.deck.pop()
+        self.user_cards.append(card)
